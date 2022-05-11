5d441044d7036b1ae11016b77b2b2079bddd3dbc
vmuhammad <105137165+vmuhammad@users.noreply.github.com>
2022-05-11 04:57:19 +0430

    Add files via upload

---
+++ random.py
@@ -0,0 +1,14 @@
+import random
+a=random.randint(1,1000)
+
+
+x=input('lotfan addad bede beyn 1 ta 1000:')
+if x ==a:
+    print('you are a winner')
+else:
+    print('try again')
+          
+
+     
+
+
